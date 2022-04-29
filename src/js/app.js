eaee924f95c6956a05334794e71400f839b97c4c
Dmitriy Sopp <d.sopp@groupstp.ru>
2022-04-29 09:43:22 +0800

    адаптив

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,23 @@ flsFunctions.isWebp()
 
 const burger =  document.querySelector('.header__burger')
 const menu =  document.querySelector('.header__menu')
+const links = document.querySelectorAll('.header__link')
+links.forEach(link => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault()
+    const id = link.getAttribute('href').substring(1)
+    const section = document.getElementById(id)  
+
+    if(section) {
+      seamless.scrollIntoView(section, {
+        behavior: "smooth",
+        top: 100
+      })
+    }
+    burger.classList.remove('active')
+    menu.classList.remove('active')
+  })
+})
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active')
